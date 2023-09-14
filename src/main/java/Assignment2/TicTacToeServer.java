0788b5f109f066528b857bdc50793d9153510d60
SamMacbokAir <w304714766@gmail.com>
2023-09-14 20:33:53 +1000

    Assignment 2 basic architecture

--- src/main/java/Assignment2/TicTacToeServer.java
+++ src/main/java/Assignment2/TicTacToeServer.java
@@ -1,23 +1,26 @@
 package Assignment2;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.*;
 
 public class TicTacToeServer {
     private static final int PORT = 8080;
+    private static final int MAX_PLAYERS = 100;  // Adjust based on your expected number of concurrent players
     public static ConcurrentLinkedQueue<Player> waitingPlayers = new ConcurrentLinkedQueue<>();
+    private static ExecutorService threadPool = Executors.newFixedThreadPool(MAX_PLAYERS);
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("TicTacToe Server started on port " + PORT);
             while (true) {
-                new Player(serverSocket.accept()).start();
+                Player player = new Player(serverSocket.accept());
+                threadPool.execute(player);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
 
