4e6cd08a6d6751fcdcdfb8e4ae4cf59e23c2954a
SamMacbokAir <w304714766@gmail.com>
2023-09-15 18:50:46 +1000

    Assignment 2 day 3-3

---
+++ src/main/java/Assignment2/GameState.java
@@ -0,0 +1,10 @@
+package Assignment2;
+
+public class GameState {
+    public final int WAITING_FOR_PLAYER = 0;
+    public final int IN_PROGRESS = 1;
+    public final int PLAYER_X_WON = 2;
+    public final int PLAYER_O_WON = 3;
+    public final int DRAW = 4;
+
+}
